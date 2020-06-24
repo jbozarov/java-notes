b7040040184b9aeab8ed60295fc35f7977f9e2fe
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-24 11:20:56 -0400

    Update General.java

--- General.java
+++ General.java
@@ -4,3 +4,5 @@ METHOD OVERLOADING / OVERRIDING
 Method overriding is child class uses own method which is a same as name of parent class's method
 
 Method overloading ( happens within the class ) so class uses same name to create multiple methods with different parameters 
+  
+Concrete method is method has a body definition, opposite of abstract method 
