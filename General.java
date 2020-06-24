56ae305895a01d8eb162e4830abd2127b2ac78cd
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-24 12:23:36 -0400

    Update General.java

--- General.java
+++ General.java
@@ -1,8 +1,38 @@
 
 METHOD OVERLOADING / OVERRIDING 
 
-Method overriding is child class uses own method which is a same as name of parent class's method
+Method overriding is child class uses own method which is a same as name of parent class's method ( you change ) 
 
 Method overloading ( happens within the class ) so class uses same name to create multiple methods with different parameters 
+  ( Amir said same name but different parameters  
+  JS has not overloading, last method will be invoked But Java will invoke the one which number of parameters match 
+  
   
 Concrete method is method has a body definition, opposite of abstract method 
+
+Abstaraction defining method without body and whoever wants to use they can define the method body and use it 100% abstraction is Interfaces 
+
+Polymorphism using same method multiple times from child classes to 
+
+Inheritence child class uses parent class methods properties using extends 
+   
+   
+   Association => One class is related to another class
+   IS A => inheritance Car IS A vehicle, Dog IS A(n) Animal
+   HAS A => Car HAS A Engine 
+   composition 
+   aggregation 
+   School is a Building, School has a library 
+   
+   
+   Access modifiers private public protected package 
+   Non access modifiers 
+   static no instance
+   final const in js
+   abstract thread safety
+   synchronized(multithreading) 
+   
+   
+  
+
+
