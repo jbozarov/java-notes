50ded9bc40e6d9b92af9323ad582836032bfc27f
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-07-13 23:52:03 -0400

    Update General.java

--- General.java
+++ General.java
@@ -54,6 +54,19 @@
        That's where you want to use protected. When you want only inherited classes to be able to access 
        your class. It's a neat little way to hide stuff from unrelated classes
 
-
+                                               
+                                               
+                                              
+                                              
+OOP Design Patterns 
+Creational patterns are design patterns that deal with object creation mechanisms and are used in situations when basic form of object creation 
+could result in design problems or increase complexity of a code base.
+                                               
+Structural design patterns are design patterns that ease the design by identifying a simple way to realise relationships between entities or defines a 
+manner for creating relationships between objects.       
+                                               
+Behavioral pattern explains how objects interact. It describes how different objects and classes send messages to each other to make things happen 
+and how the steps of a task are divided among different objects. Where Creational patterns mostly describe a moment of time (the instant of creation), 
+and Structural patterns describe a more or less static structure, Behavioral patterns describe a process or a flow                                               
 
 
