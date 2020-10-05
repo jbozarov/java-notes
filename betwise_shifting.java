18dbfa19e6a63a9c7684df44d066b0997860969d
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-10-04 20:16:12 -0400

    Create betwise_shifting.java

---
+++ betwise_shifting.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Shifting {
+
+	public static void main(String[] args) {
+		System.out.println(9>>2);
+		System.out.println(9>>1);
+		System.out.println(9>>0);
+		
+		System.out.println(9<<1); // 18 shifting to the right: 
+		//  9 is:										1  0  0  1
+		//  shifting to the left <<                  1  0  0  1  0  we shifted each number to the right and 10010 is 18 in decimal
+		
+		System.out.println(9>>>1);
+	}
+}
