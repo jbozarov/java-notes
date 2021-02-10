ce1803fbc3181913b93b7c181aec32c7791f4cfb
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-02-10 08:26:01 -0600

    Update Main.java

--- predicate/Main.java
+++ predicate/Main.java
@@ -11,6 +11,8 @@ public class Main {
         PredicateDemo predicate = new PredicateDemo();
         
         numbers.stream().filter(predicate).forEach(System.out::println); // 2, 4, 6, 8
+        
+        numbers.stream().filter(v -> v == 2).forEach(System.out::println); // 2
 
     }
 }
