761263d895c32a95f0fc37d1b43a6345d2f1c8c9
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-02-09 21:19:37 -0600

    Update Main.java

--- consumer/Main.java
+++ consumer/Main.java
@@ -5,10 +5,35 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        
         List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6);
         ConsumerDemo consumer = new ConsumerDemo();
-        integerList.forEach(consumer);
+        
+        integerList.forEach(consumer); 
+//         Integer item is 1
+//         Integer item is 2
+//         Integer item is 3
+//         Integer item is 4
+//         Integer item is 5
+//         Integer item is 6
+        
+        
         integerList.forEach(System.out::println);
+//         1
+//         2
+//         3
+//         4
+//         5
+//         6
+
+        
         integerList.forEach(t -> System.out.println("Type is " + t));
+//         Type is 1
+//         Type is 2
+//         Type is 3
+//         Type is 4
+//         Type is 5
+//         Type is 6
+        
     }
 }
