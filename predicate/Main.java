3f5c7a8b7e30099d56761e3c2a2930ed2230bd9b
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-02-09 21:26:49 -0600

    Update Main.java

--- predicate/Main.java
+++ predicate/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8);
-    PredicateDemo predicate = new PredicateDemo();
-
-    numbers.stream().filter(predicate).forEach(System.out::println);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        
+        PredicateDemo predicate = new PredicateDemo();
+        
+        numbers.stream().filter(predicate).forEach(System.out::println); // 2, 4, 6, 8
 
     }
 }
