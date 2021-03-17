8405cb9d0ee1eaef713a26e15e266d581caf57ff
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-03-16 19:14:01 -0500

    Create Project.java

---
+++ sort-comparator/Project.java
@@ -0,0 +1,37 @@
+package com.company.sort_compare;
+
+public class Project {
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Project(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
