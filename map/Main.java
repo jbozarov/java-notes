931d98027c93480449b5ad3052480a0ad49d2c59
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-03-28 08:45:54 -0500

    Update and rename MapDemoTwo.java to map/Main.java

--- map/Main.java
+++ map/Main.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class MapDemoTwo {
+public class Main {
 
     public static void main(String[] args) {
         mapExamples();
